97c01e79a77c39ad521ff46024c1657e211af1eb
RIchard Glaser <richard.glaser@utah.edu>
2025-07-01 15:02:46 -0600

    espanso-updated-fixed-label
    output...
    ```
    # sudo Installomator/utils/assemble.sh espanso DEBUG=0
    2025-07-01 15:02:00 : INFO  : espanso : setting variable from argument DEBUG=0
    2025-07-01 15:02:00 : INFO  : espanso : Total items in argumentsArray: 1
    2025-07-01 15:02:00 : INFO  : espanso : argumentsArray: DEBUG=0
    2025-07-01 15:02:00 : REQ   : espanso : ################## Start Installomator v. 10.9beta, date 2025-07-01
    2025-07-01 15:02:00 : INFO  : espanso : ################## Version: 10.9beta
    2025-07-01 15:02:00 : INFO  : espanso : ################## Date: 2025-07-01
    2025-07-01 15:02:00 : INFO  : espanso : ################## espanso
    2025-07-01 15:02:02 : INFO  : espanso : Reading arguments again: DEBUG=0
    2025-07-01 15:02:02 : INFO  : espanso : BLOCKING_PROCESS_ACTION=tell_user
    2025-07-01 15:02:02 : INFO  : espanso : NOTIFY=success
    2025-07-01 15:02:02 : INFO  : espanso : LOGGING=INFO
    2025-07-01 15:02:02 : INFO  : espanso : LOGO=/System/Applications/App Store.app/Contents/Resources/AppIcon.icns
    2025-07-01 15:02:02 : INFO  : espanso : Label type: dmg
    2025-07-01 15:02:02 : INFO  : espanso : archiveName: Espanso-Mac-M1.dmg
    2025-07-01 15:02:02 : INFO  : espanso : name: Espanso, appName: Espanso.app
    2025-07-01 15:02:02 : INFO  : espanso : App(s) found: /Applications/Utilities/Admin/Espanso.app
    2025-07-01 15:02:02 : INFO  : espanso : found app at /Applications/Utilities/Admin/Espanso.app, version 2.2.3, on versionKey CFBundleShortVersionString
    2025-07-01 15:02:02 : INFO  : espanso : appversion: 2.2.3
    2025-07-01 15:02:02 : INFO  : espanso : Latest version of Espanso is 2.2.3
    2025-07-01 15:02:02 : INFO  : espanso : There is no newer version available.
    2025-07-01 15:02:02 : INFO  : espanso : Installomator did not close any apps, so no need to reopen any apps.
    2025-07-01 15:02:02 : REQ   : espanso : No newer version.
    2025-07-01 15:02:02 : REQ   : espanso : ################## End Installomator, exit code 0
    ```

--- fragments/labels/espanso.sh
+++ fragments/labels/espanso.sh
@@ -1,13 +1,9 @@
 espanso)
     name="Espanso"
-    type="zip"
-    if [[ "$(arch)" == "arm64" ]]; then
-        archiveName="Espanso-Mac-M1.zip"
-    else
-        archiveName="Espanso-Mac-Intel.zip"
-    fi
+    type="dmg"
+    archiveName="Espanso-Mac-M1.dmg"
     downloadURL="$(downloadURLFromGit espanso espanso)"
     appNewVersion="$(versionFromGit espanso espanso)"
     blockingProcesses=( "Espanso" "espanso" )
-    expectedTeamID="K839T4T5BY"
+    expectedTeamID="6424323YUH"
     ;;
